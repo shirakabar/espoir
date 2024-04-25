518492b72d1c7072a5548d67c28066cff03468e6
yoropiko3292 <shuma.ikeda@outlook.jp>
2024-04-25 23:50:57 +0900

    ちずですよー

---
+++ koyo/lib/firstfloor.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+//博覧ページのタブ遷移先
+
+class FirstFloor extends StatelessWidget {
+  const FirstFloor({super.key});
+  
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          // Positionedで配置
+          Positioned(
+            left: 10,
+            top: 40,
+            width: 400,
+            height: 250,
+            child: Image.asset('images/1f.png'),
+          ),
+          // Positionedで配置
+        ],
+      ),
+    );
+  }
+}
